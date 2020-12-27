4b9f680c4e3793db5bc6dfe89e3a4e99a3600c84
Brent Rodriguez <b.rod-code@Brents-MBP.attlocal.net>
2020-12-27 15:15:26 -0600

    Update Game Logic

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 
 const message = document.querySelector('.message')
 const number = document.querySelector('.number')
-const score = document.querySelector('.score')
 const guess = document.querySelector('.guess')
+
+// MUTABLES
+let score = document.querySelector('.score')
 
 // CHECK BUTTON
 const checkBtn = document.querySelector('.check')
@@ -11,6 +13,11 @@ const checkBtn = document.querySelector('.check')
 
 // GENERATES RANDOM NUMBER
 const ranNumber = Math.trunc(Math.random() * 20) +1
+
+
+
+
+let updateScore = 20
 number.textContent = ranNumber
 
 
@@ -18,13 +25,17 @@ number.textContent = ranNumber
 checkBtn.addEventListener('click', () => {
   const inputValue = Number(guess.value)
 
-  if(!inputValue){
+  if(!inputValue){ // CHECKS FOR NO INPUT
     message.textContent = '🛑 Please add a Number!'
   } else if(inputValue === ranNumber){
     message.textContent = '🏆 Congrats Number Matched'
   } else if(inputValue > ranNumber){
     message.textContent = '😫 Guess is too high'
+    updateScore--
+    score.textContent = updateScore
   } else if(inputValue < ranNumber){
     message.textContent = '😫 Guess is too Low'
+    updateScore--
+    score.textContent = updateScore
   }
 })
