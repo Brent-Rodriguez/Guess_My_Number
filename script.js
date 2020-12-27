a59892dd21c1c0647e1e073ec605e3b5250aaeaa
Brent Rodriguez <b.rod-code@Brents-MBP.attlocal.net>
2020-12-27 14:08:14 -0600

    Add Event

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 'use strict'
 
 const message = document.querySelector('.message')
+const number = document.querySelector('.number')
+const score = document.querySelector('.score')
+const guess = document.querySelector('.guess')
 
+const chkBtn = document.querySelector('.check')
 
-
-message.textContent = '🎊 Correct Answer'
+// 
+chkBtn.addEventListener('click', () => {
+  console.log(guess.value)
+})
